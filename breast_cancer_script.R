ca3acfabd48ae33aa96422a9281139d6b9ed86b4
rachaeldiazsanz <rdiazsan@oberlin.edu>
2025-06-12 10:47:11 -0400

    Removed the male cases

--- breast_cancer_script.R
+++ breast_cancer_script.R
@@ -4,4 +4,4 @@ library(readr)
 breast_cancer_data_new <- read_csv("Desktop/2025-osri-breast-cancer/breast_cancer_data_new.csv")
 
 # remove male variable
-breast_cancer_data_clean = 
+breast_cancer_data_clean = subset(breast_cancer_data_new, breast_cancer_data_new$Sex != "Male")
