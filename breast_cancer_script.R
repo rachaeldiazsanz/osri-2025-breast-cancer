d39bd6b354faf20055cf0ebfe5ed5a43b1b06818
Munzalin402 <mmozumda@oberlin.edu>
2025-06-12 10:40:05 -0400

    Added comments

--- breast_cancer_script.R
+++ breast_cancer_script.R
@@ -1,2 +1,7 @@
 library(readr)
+
+# reading the file
 breast_cancer_data_new <- read_csv("Desktop/2025-osri-breast-cancer/breast_cancer_data_new.csv")
+
+# remove male variable
+breast_cancer_data_clean = 
