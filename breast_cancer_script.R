857556c942323c965aace3f5342ac8452be4b0d2
rachaeldiazsanz <rdiazsan@oberlin.edu>
2025-06-12 11:16:05 -0400

    removed instances of women over 40

--- breast_cancer_script.R
+++ breast_cancer_script.R
@@ -3,5 +3,20 @@ library(readr)
 # reading the file
 breast_cancer_data_new <- read_csv("Desktop/2025-osri-breast-cancer/breast_cancer_data_new.csv")
 
-# remove male variable
+# remove male instances
 breast_cancer_data_clean = subset(breast_cancer_data_new, breast_cancer_data_new$Sex != "Male")
+
+# Only include women under 40 years of age
+breast_cancer_data_clean <- subset(
+  breast_cancer_data_clean,
+  `Age recode with <1 year olds and 90+` %in% c(
+    "01-04 years",
+    "05-09 years",
+    "10-14 years",
+    "15-19 years",
+    "20-24 years",
+    "25-29 years",
+    "30-34 years",
+    "35-39 years"
+  )
+)
