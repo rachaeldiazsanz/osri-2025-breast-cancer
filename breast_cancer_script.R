a9d37bbb7b6263a22ec9ffab72866e9a7eac2d33
rachaeldiazsanz <rdiazsan@oberlin.edu>
2025-06-17 12:46:00 -0400

    recoded the cause of death

--- breast_cancer_script.R
+++ breast_cancer_script.R
@@ -28,14 +28,25 @@ breast_cancer_data_clean = breast_cancer_data_clean[,-c(2, 30, 32, 36)]
 # Removing unknown income entries
 breast_cancer_data_clean = subset(breast_cancer_data_clean, breast_cancer_data_clean$`Median household income inflation adj to 2023` != "Unknown/missing/no match/Not 1990-2023")
 
+# Recoding income based on guidelines found in paper
 breast_cancer_data_clean <- breast_cancer_data_clean %>%
   mutate(
-    income_group = case_when(
+    `Median household income inflation adj to 2023` = case_when(
       `Median household income inflation adj to 2023` %in% c("< $40,000", "$40,000 - $44,999", "$45,000 - $49,999") ~ "<$50,000",
       `Median household income inflation adj to 2023` %in% c("$50,000 - $54,999", "$55,000 - $59,999", "$60,000 - $64,999", "$65,000 - $69,999") ~ "$50,000-$74,999",
       `Median household income inflation adj to 2023` %in% c("$70,000 - $74,999", "$75,000 - $79,999", "$80,000 - $84,999", "$85,000 - $89,999") ~ "$75,000-$99,999",
       `Median household income inflation adj to 2023` %in% c("$90,000 - $94,999", "$95,000 - $99,999") ~ "$75,000-$99,999",
       `Median household income inflation adj to 2023` %in% c("$100,000 - $109,999", "$110,000 - $119,999", "$120,000+") ~ ">= $100,000",
+      TRUE ~ NA_character_
+    )
+  )
+
+# Recoding cause of death to censor for survival analysis
+breast_cancer_data_clean <- breast_cancer_data_clean %>%
+  mutate(
+    `SEER cause-specific death classification` = case_when(
+      `SEER cause-specific death classification` %in% c("Alive or dead of other cause", "Dead (missing/unknown COD)") ~ "0",
+      `SEER cause-specific death classification` %in% c("Dead (attributable to this cancer dx)") ~ "1",
       TRUE ~ NA_character_
     )
   )
